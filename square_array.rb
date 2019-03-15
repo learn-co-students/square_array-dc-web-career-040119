cc74d7838832898c98b2bc339ec404443da8ff6d
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-03-15 15:36:05 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,11 @@
 def square_array(array)
   # your code here
+  sq_array= []
+  arr_size= array.size
+  count = 0
+  array.each do |val|
+    sq_val= val ** 2
+    array[count] = sq_val
+    count += 1
+  end
 end
